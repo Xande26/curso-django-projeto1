5208c5c306cffe8b0758f951cff2e137490d030d
Alexandre de Araújo <xandeagfilho@gmail.com>
2025-09-03 19:04:25 -0300

    Updates from Forms

--- project/settings.py
+++ project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # O nossos apps
     'recipes',
+    'authors',
 ]
 
 MIDDLEWARE = [
--- project/urls.py
+++ project/urls.py
@@ -23,6 +23,7 @@ from django.conf import settings  # type: ignore
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls')),
+    path('authors/', include('authors.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
