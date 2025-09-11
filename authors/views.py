d1f8f7d062e1d34e9f7aacf5dcbadbdbe592329c
Alexandre de Araújo <xandeagfilho@gmail.com>
2025-09-11 19:25:50 -0300

    Updates form Login

--- authors/views.py
+++ authors/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
-from . forms import RegisterForm
+from . forms import RegisterForm, LoginForm
 from django.http import Http404
 from django.contrib import messages
 from django.urls import reverse
+from django.contrib.auth import authenticate, login
 
 
 def register_view(request):
@@ -10,7 +11,7 @@ def register_view(request):
     form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
-        'form_action': reverse("authors:create"),
+        'form_action': reverse("authors:register_create"),
     })
 
 
@@ -29,5 +30,36 @@ def register_create(request):
         messages.success(request, 'Your users is create, please log in.')
 
         del(request.session['register_form_data'])
-        
+   
     return redirect('authors:register')
+
+
+def login_view(request):
+    form = LoginForm()
+    return render(request, 'authors/pages/login.html', {
+        'form': form,
+        'form_action': reverse('authors:login_create')
+    })
+
+
+def login_create(request):
+    if not request.POST:
+        raise Http404()
+    form = LoginForm(request.POST)
+    login_url = reverse('authors:login')
+
+    if form.is_valid():
+        authenticate_user = authenticate(
+            username=form.cleaned_data.get('username', ''),
+            password=form.cleaned_data.get('password', ''),
+        )
+
+        if authenticate_user is not None:
+            messages.success(request, 'Yuor are logged in.')
+            login(request, authenticate_user)
+        else:
+            messages.error(request, 'Invalid credentials')
+    else:
+        messages.error(request, 'Invalid username or password')
+
+    return redirect(login_url)
